7ec59d6a2f9dae93f1152aea7ad35d5c56063d4c
Aaron Kirchman <65667835+akirchman16@users.noreply.github.com>
2022-01-17 20:58:42 -0600

    Update Competiton_RPA_Diffusion_010922.m

--- Competiton_RPA_Diffusion_010922.m
+++ Competiton_RPA_Diffusion_010922.m
@@ -15,7 +15,7 @@ minIterations = 100;
 %RAD51 Properties/Parameters
 RAD51 = 51;     %how RAD51 will be represented on the lattice
 n_RAD51 = 3;    %size of RAD51 protein
-TotalCount_RAD51 = 300; %number of total RAD51 proteins (monomers)
+TotalCount_RAD51 = 50; %number of total RAD51 proteins (monomers)
 w_RAD51 = 1;    %cooperativity constant for RAD51
 k_on_RAD51 = 0.1; %kinetic rate constant for RAD51 binding
 k_off_RAD51 = 10;    %kinetic rate constant for RAD51 unbinding
@@ -26,7 +26,7 @@ RPA_D = 3;  %represent RPA-D on lattice
 n_A = 10;   %size of RPA-A
 n_D = 10;   %size of RPA-D
 n_RPA = n_A+n_D;
-TotalCount_RPA = 50;    %total number of RPA proteins that exist
+TotalCount_RPA = 10;    %total number of RPA proteins that exist
 w_RPA = 1;  %cooperativity of RPA (IDK if this is fully included in the model currently)
 k_on_RPA_A = 100;    %kinetic rate constant for RPA-A binding
 k_off_RPA_A = 5;    %kinetic rate constant for RPA-A unbinding
@@ -34,7 +34,7 @@ k_on_RPA_D = 30;    %kinetic rate consant for RPA-D binding
 k_off_RPA_D = 10;    %kinetic rate constant for RPA-D unbinding
 
 DiffusionRate = 100000;    %RPA Diffusion Rate constant (events/time interval)
-Left_Prob = 0.8;    %probability of left diffusion, when both are possible (value between 0 and 1)
+Left_Prob = 0.1;    %probability of left diffusion, when both are possible (value between 0 and 1)
 Right_Prob = 1-Left_Prob;
 
 %Memory Allocation
